ee82e52637666ecbb2476bb8a04329ca9df09bb2
musainfinixej <mosesinfinixej@gmail.com>
2026-05-17 12:13:57 +0700

    fitur stok

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+
 const {
   default: makeWASocket,
   useMultiFileAuthState
@@ -44,6 +46,72 @@ async function startBot() {
   });
 
   sock.ev.on("creds.update", saveCreds);
+
+  // =========================
+  // FITUR !stok
+  // =========================
+
+  sock.ev.on("messages.upsert", async ({ messages }) => {
+
+    const msg = messages[0];
+
+    if (!msg.message) return;
+
+    const text =
+      msg.message.conversation || "";
+
+    const from = msg.key.remoteJid;
+
+    console.log("PESAN MASUK:", text);
+
+    // COMMAND
+    if (text === "!stok") {
+
+      try {
+
+        // Spreadsheet
+        const doc = new GoogleSpreadsheet(
+          "1-YH8NuZBQ86nUVPF-tzwIsR6qNrX1jLZQhpILWVy-Tg"
+        );
+
+        // API KEY
+        await doc.useApiKey(
+          "AIzaSyAPEbgpe0g8bbHnVI3b-ixfw9CPioVrS-E"
+        );
+
+        // Load Spreadsheet
+        await doc.loadInfo();
+
+        // Sheet pertama
+        const sheet = doc.sheetsByIndex[0];
+
+        // Ambil semua row
+        const rows = await sheet.getRows();
+
+        let reply = "*DATA STOK*\n\n";
+
+        rows.forEach((row) => {
+
+          reply +=
+            `${row.Barang} - ${row.Stok}\n`;
+
+        });
+
+        // Kirim balasan
+        await sock.sendMessage(from, {
+          text: reply
+        });
+
+      } catch (err) {
+
+        console.log("ERROR:", err);
+
+        await sock.sendMessage(from, {
+          text: "Gagal mengambil data spreadsheet"
+        });
+      }
+    }
+  });
 }
 
 startBot();
